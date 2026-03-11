ec4b5cb7877a3095e060ccdd43718a9c52195e7b
nonom <nonomartinez@gmail.com>
2026-03-11 18:55:08 +0100

    Make build.

--- compose.bat
+++ compose.bat
@@ -11,8 +11,6 @@ if "%~1"=="" (
 if /I "%cmd%"=="up" (
   call :set_compose_files
   docker compose !compose_files! up -d %*
-  if errorlevel 1 goto :eof
-  docker compose !compose_files! logs -f
   goto :eof
 )
 if /I "%cmd%"=="down" (
@@ -31,48 +29,41 @@ if /I "%cmd%"=="ps" (
   goto :eof
 )
 if /I "%cmd%"=="build" (
-  call :set_compose_files
+  call :set_compose_build_files
   docker compose !compose_files! build %*
   goto :eof
 )
 if /I "%cmd%"=="recreate" (
   call :set_compose_files
-  docker compose !compose_files! down
+  docker compose !compose_files! down --remove-orphans
   if errorlevel 1 goto :eof
   docker compose !compose_files! up -d --force-recreate %*
-  if errorlevel 1 goto :eof
-  docker compose !compose_files! logs -f
   goto :eof
 )
-
 if /I "%cmd%"=="rebuild" (
+  call :set_compose_build_files
+  docker compose !compose_files! build --no-cache %*
+  if errorlevel 1 goto :eof
   call :set_compose_files
-  docker compose !compose_files! build %*
-  if errorlevel 1 goto :eof
   docker compose !compose_files! up -d --force-recreate %*
-  if errorlevel 1 goto :eof
-  docker compose !compose_files! logs -f
   goto :eof
 )
-
 if /I "%cmd%"=="restart" (
   call :set_compose_files
   docker compose !compose_files! down --remove-orphans
   if errorlevel 1 goto :eof
   docker compose !compose_files! up -d %*
-  if errorlevel 1 goto :eof
-  docker compose !compose_files! logs -f
   goto :eof
 )
-
 if /I "%cmd%"=="test" (
-  for /f "delims=" %%F in ('where /r tests compose.yml 2^>nul ^| sort') do (
-    docker compose -f docker-compose.yml -f "%%~fF" run --rm test %*
-    if errorlevel 1 goto :eof
-  )
+  call :set_compose_files
+  docker compose !compose_files! -f tests/db/compose.yml run --rm test-db %*
+  if errorlevel 1 goto :eof
+  docker compose !compose_files! -f tests/login/compose.yml run --rm test-login %*
+  if errorlevel 1 goto :eof
+  docker compose !compose_files! -f tests/game/compose.yml run --rm test-game %*
   goto :eof
 )
-
 
 echo Unknown command: %cmd%
 echo Usage: %~n0 ^<up^|down^|logs^|ps^|build^|recreate^|rebuild^|restart^|test^> [docker compose args]
@@ -81,6 +72,16 @@ exit /b 1
 :set_compose_files
 set "compose_files=-f docker-compose.yml"
 for /f "delims=" %%F in ('where /r server compose.yml 2^>nul ^| sort') do (
+  set "compose_files=!compose_files! -f ""%%~fF"""
+)
+goto :eof
+
+:set_compose_build_files
+set "compose_files=-f docker-compose.yml"
+for /f "delims=" %%F in ('where /r server compose.yml 2^>nul ^| sort') do (
+  set "compose_files=!compose_files! -f ""%%~fF"""
+)
+for /f "delims=" %%F in ('where /r build compose.yml 2^>nul ^| sort') do (
   set "compose_files=!compose_files! -f ""%%~fF"""
 )
 goto :eof
